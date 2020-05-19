2093a54ef7ef4a52256458331b4eab6a3c8e6747
Laurie Barth <laurie@LauriesrkLaptop.fios-router.home>
2020-05-19 10:11:05 -0400

    Revert "Listing page and new post"
    This reverts commit 138cd8f2c5cd5b1d0e350d8a5a50d5a74b56b798.

--- content/posts/egghead/index.md
+++
@@ -1,5 +0,0 @@
----
-title: All about Egghead
----
-
-Egghead is a fantastic learning resource for developers!
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,10 @@
 import React from 'react'
 import Layout from '../components/layout'
-import {graphql, Link} from 'gatsby'
 
-
-export const HomePage = ({data}) => {
-    const posts = data.allMarkdownRemark.nodes
-    return (
+export const HomePage = () => (
     <Layout>
-    {posts.map((post) => {
-        return (<Link to={post.fields.slug} key={post.fields.slug}>
-        <h2>{post.frontmatter.title}</h2>
-        </Link>
-        )
-            })}
+        <h2>Hello World</h2>
     </Layout>
-)}
-
-export const pageQuery = graphql`
-query { 
-    allMarkdownRemark(sort: { fields: [frontmatter___title], order: ASC }) {
-    nodes {
-        fields {
-            slug
-        }
-        frontmatter {
-          title
-        }
-      }
-  }
-}
-`
+)
 
 export default HomePage
