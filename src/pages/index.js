94d82ee6989242b07e94d6de0dd26ae105488ad8
Laurie Barth <laurie@LauriesrkLaptop.fios-router.home>
2020-07-09 11:35:27 -0400

    update for unified routes and mdx

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,33 @@
+  
 import React from 'react'
 import Layout from '../components/layout'
+import {graphql, Link} from 'gatsby'
 
-export const HomePage = () => (
+
+export const HomePage = ({data}) => {
+    const posts = data.allMdx.nodes
+    return (
     <Layout>
-        <h2>Hello World</h2>
+    {posts.map((post) => {
+        return (<Link to={post.path} key={post.path}>
+        <h2>{post.frontmatter.title}</h2>
+        </Link>
+        )
+            })}
     </Layout>
-)
+)}
+
+export const pageQuery = graphql`
+query { 
+    allMdx(sort: { fields: [frontmatter___title], order: ASC }) {
+    nodes {
+        path(filePath: "{frontmatter__title}")
+        frontmatter {
+          title
+        }
+      }
+  }
+}
+`
 
 export default HomePage
